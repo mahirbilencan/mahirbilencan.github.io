9970515e06a19ea92e84c3fe4ba3f60a7247e5e3
EliNaig <elicoe05@icloud.com>
2026-03-01 15:13:21 -0600

    Add Google Scholar and academicons social links

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -7,6 +7,10 @@ export const site = {
 	affiliation: "Tulane University",
 	profileImage: "/images/Mahir.jpg",
 	social: {
+		orcid: "https://orcid.org/0000-0002-0175-4897",
+		arxiv:
+			"https://arxiv.org/search/math?query=mahir+bilen+can&searchtype=author&abstracts=show&order=-announced_date_first&size=50",
+		googleScholar: "https://scholar.google.com/citations?user=jbvQXE4AAAAJ&hl=en&oi=ao",
 		github: "",
 		linkedin: "",
 		youtube: "",
